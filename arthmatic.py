219a08c42b8aa5833ef32369eb3441629aabf03d
vikram7838 <ranavikram603@gmail.com>
2025-04-04 11:25:27 +0530

    hi

--- arthmatic.py
+++ arthmatic.py
@@ -1,10 +1,23 @@
-dinner = 2000
-coffee = 400
-softDrink = 440
-burger = 200
-fries = 40
-sum = dinner + coffee + softDrink + burger + fries
-print("Total Bill",sum)
-print("4% Total Bill",sum/4)
-print(sum/4*4)
+# dinner = 2000
+# coffee = 400
+# softDrink = 440
+# burger = 200
+# fries = 40
+# sum = dinner + coffee + softDrink + burger + fries
+# print("Total Bill",sum)
+# print("4% Total Bill",sum/4)
+# print(sum/4*4)
+
+hardDisk = 3500
+motherboard = 6000
+i3_cpu = 8000
+ram_16gb = 2250
+mouse = 400
+keyboard = 600
+microteck_ups = 2250
+cabinet = 1500
+scanner = 5600
+printer = 5600
+sum = hardDisk + motherboard + i3_cpu + ram_16gb + mouse + keyboard + microteck_ups + cabinet + scanner + printer
+print("total bill",sum)
 
