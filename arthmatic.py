6d318e58f4031cbfde3e9cf808509af911815396
vikram7838 <ranavikram603@gmail.com>
2025-04-04 11:16:39 +0530

    arith

---
+++ arthmatic.py
@@ -0,0 +1,10 @@
+dinner = 2000
+coffee = 400
+softDrink = 440
+burger = 200
+fries = 40
+sum = dinner + coffee + softDrink + burger + fries
+print("Total Bill",sum)
+print("4% Total Bill",sum/4)
+print(sum/4*4)
+
